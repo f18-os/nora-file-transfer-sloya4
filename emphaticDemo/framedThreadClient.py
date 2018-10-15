eebbedd181ac33f122679c43e7a6c025e8adc69c
sloya4@miners.utep.edu <sloya4@miners.utep.edu>
2018-10-15 16:36:53 -0600

    handling of multiple file upload

--- emphaticDemo/framedThreadClient.py
+++ emphaticDemo/framedThreadClient.py
@@ -1,7 +1,7 @@
 #! /usr/bin/env python3
 
 # Echo client program
-import socket, sys, re
+import socket, sys, re, os
 import params
 from framedSock import FramedStreamSock
 from threading import Thread
@@ -11,6 +11,7 @@ switchesVarDefaults = (
     (('-s', '--server'), 'server', "localhost:50001"),
     (('-d', '--debug'), "debug", False), # boolean (set if present)
     (('-?', '--usage'), "usage", False), # boolean (set if present)
+    (('put', '--put'),"put", "fileName"),
     )
 
 
@@ -18,6 +19,7 @@ progname = "framedClient"
 paramMap = params.parseParams(switchesVarDefaults)
 
 server, usage, debug  = paramMap["server"], paramMap["usage"], paramMap["debug"]
+fileName = paramMap["put"]
 
 if usage:
     params.usage()
@@ -32,10 +34,12 @@ except:
 
 class ClientThread(Thread):
     def __init__(self, serverHost, serverPort, debug):
+        global fileName
         Thread.__init__(self, daemon=False)
         self.serverHost, self.serverPort, self.debug = serverHost, serverPort, debug
         self.start()
     def run(self):
+       global fileName
        s = None
        for res in socket.getaddrinfo(serverHost, serverPort, socket.AF_UNSPEC, socket.SOCK_STREAM):
            af, socktype, proto, canonname, sa = res
@@ -69,6 +73,21 @@ class ClientThread(Thread):
 
        fs.sendmsg(b"hello world")
        print("received:", fs.receivemsg())
+       
+       if os.path.isfile(fileName):#Check if file exists
+           fileR = open(fileName, "rb")
+           fileName1 = "IFL$" +fileName # IFL$ To let the server know it is a file
+           fs.sendmsg(fileName1.encode())# Send file name
+           receiveP = fs.receivemsg()
+           print("received: ", receiveP)
+           if receiveP.decode() != "File already in server":
+               read = fileR.read(100)
+               while read:
+                   read = read.decode().strip().encode()
+                   fs.sendmsg(read)
+                   print("received: ", fs.receivemsg())
+                   read = fileR.read(100)
+               fileR.close()
 
 for i in range(100):
     ClientThread(serverHost, serverPort, debug)
--- emphaticDemo/framedThreadServer.py
+++ emphaticDemo/framedThreadServer.py
@@ -1,5 +1,5 @@
 #! /usr/bin/env python3
-import sys, os, socket, params, time
+import sys, os, socket, params, time, threading
 from threading import Thread
 from framedSock import FramedStreamSock
 
@@ -13,6 +13,7 @@ progname = "echoserver"
 paramMap = params.parseParams(switchesVarDefaults)
 
 debug, listenPort = paramMap['debug'], paramMap['listenPort']
+secure = threading.Lock()
 
 if paramMap['usage']:
     params.usage()
@@ -25,23 +26,44 @@ print("listening on:", bindAddr)
 
 class ServerThread(Thread):
     requestCount = 0            # one instance / class
-    def __init__(self, sock, debug):
+    def __init__(self, sock, debug): 
         Thread.__init__(self, daemon=True)
         self.fsock, self.debug = FramedStreamSock(sock, debug), debug
         self.start()
     def run(self):
-        while True:
+        while True:#While the client keeps sending messages
             msg = self.fsock.receivemsg()
-            if not msg:
+            if not msg: #If empty, stop
                 if self.debug: print(self.fsock, "server thread done")
                 return
             requestNum = ServerThread.requestCount
             time.sleep(0.001)
             ServerThread.requestCount = requestNum + 1
+            isAFile = msg.decode().split("$")
+            if len(isAFile) > 1: #If it has a file name
+                fileName = isAFile[1].encode()
+            else:
+                fileName = msg
+            self.fsock.sendmsg(fileName)
+            if isAFile[0] == "IFL":
+                if os.path.isfile("serverFolder/"+fileName.decode()):#Check if fileName is in server
+                   self.fsock.send(b"File already in server")#File Already in Server
+                else:
+                    secure.acquire()
+                    fileW = open("serverFolder/"+fileName.decode(), "wb")
+                    while True:
+                        msg = self.fsock.receivemsg()
+                        if not msg:
+                            break
+                        fileW.write(msg)
+                        self.fsock.sendmsg(msg)
+                    fileW.close()
+                    secure.release()
+ 
             msg = ("%s! (%d)" % (msg, requestNum)).encode()
             self.fsock.sendmsg(msg)
 
 
-while True:
-    sock, addr = lsock.accept()
+while True: #Keeps the server open
+    sock, addr = lsock.accept() #Waits for a client
     ServerThread(sock, debug)
